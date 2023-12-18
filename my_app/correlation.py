6010bca3e348e873eb51e5f57673779b34dcad51
Matt Santos <mattsantos541@gmail.com>
2023-12-18 09:13:38 -0600

    Update preprocessing.py and correlation.py

--- my_app/correlation.py
+++ my_app/correlation.py
@@ -1,22 +1,52 @@
+import anvil.server
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+import io
+import base64
+import pandas as pd
 
 class Correlation:
-    def __init__(self, dataset):
-        self.dataset = dataset
+    def __init__(self):
+        self.dataset = None
+
+    def set_dataset(self, dataframe):
+        self.dataset = dataframe
 
     def generate_heatmap(self):
+        if self.dataset is None:
+            return None  # No dataset to analyze
+
+        # Create a correlation matrix
         correlation_matrix = self.dataset.corr()
+
+        # Generate a heatmap plot
         plt.figure(figsize=(10, 8))
         sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm')
         plt.title("Correlation Heat Map")
-        plt.show()
 
-    def compare_histograms(self, column):
-        plt.figure(figsize=(10, 6))
-        sns.histplot(self.dataset[column], label='Original', kde=True)
-        sns.histplot(self.dataset[column], label='Synthetic', kde=True)
-        plt.title(f"Histogram Comparison for {column}")
-        plt.legend()
-        plt.show()
+        # Save the heatmap plot to a BytesIO object
+        buffer = io.BytesIO()
+        plt.savefig(buffer, format='png')
+        buffer.seek(0)
+
+        # Encode the plot as base64 to display in Anvil
+        plot_base64 = base64.b64encode(buffer.read()).decode()
+        buffer.close()
+
+        return plot_base64
+
+# Create an instance of the Correlation module
+correlation_anvil = Correlation()
+
+# Define an Anvil server callable function to set the dataset
+@anvil.server.callable
+def set_correlation_dataset(dataframe_json):
+    dataframe = pd.read_json(dataframe_json)
+    correlation_anvil.set_dataset(dataframe)
+
+# Define an Anvil server callable function to generate the heatmap
+@anvil.server.callable
+def generate_correlation_heatmap():
+    heatmap_base64 = correlation_anvil.generate_heatmap()
+    return heatmap_base64
+
--- my_app/preprocessing.py
+++ my_app/preprocessing.py
@@ -1,76 +1,35 @@
-import pandas as pd
+import anvil.server
+from anvil import *
 
+class PreprocessingForm(PreprocessingFormTemplate):
 
-def display_dataframe_info(df):
-    print("DataFrame Info:")
-    print(df.info())
+    def __init__(self, data_table):
+        self.data_table = data_table
 
+    def show_data(self, data):
+        self.data_table.items = data.to_dict(orient="records")
 
-def preprocess_dataset(dataset):
-    # Perform data preprocessing tasks on the dataset using pandas
-    df = pd.DataFrame(dataset)  # Convert dataset to a DataFrame
+    def drop_non_numeric_columns(self):
+        data = self.data_table.items
+        for col in data.columns:
+            if not pd.api.types.is_numeric_dtype(data[col]):
+                data = data.drop(columns=[col])
 
-    # Print the first 10 rows of the DataFrame
-    print(df.head(10))
+        self.show_data(data)
 
-    display_dataframe_info(df)  # Display DataFrame info
+    def drop_selected_columns(self, columns_to_drop):
+        data = self.data_table.items
+        data = data.drop(columns=columns_to_drop)
+        self.show_data(data)
 
-    # Prompt user for dropping non-numeric columns
-    drop_non_numeric = input("Do you want to drop non-numeric columns? (y/n): ")
-    if drop_non_numeric.lower() == "y":
-        non_numeric_columns = df.select_dtypes(exclude=["number"]).columns
-        df = df.drop(columns=non_numeric_columns)
-        print("Non-numeric columns dropped.")
-        print("Updated DataFrame:")
-        print(df.head(10))
+    def handle_duplicates(self, remove_duplicates):
+        data = self.data_table.items
+        if remove_duplicates:
+            data = data.drop_duplicates()
+        self.show_data(data)
 
-    # Prompt user to select columns to drop
-    drop_columns_option = input("Do you want to drop any specific columns? (y/n): ")
-    while drop_columns_option.lower() == "y":
-        print("Columns in the DataFrame:")
-        print(df.columns)
-
-        # Prompt user to enter column numbers to drop
-        columns_to_drop = input("Enter the column numbers to drop (comma-separated): ").split(",")
-        columns_to_drop = [int(col) for col in columns_to_drop]
-
-        # Drop the specified columns
-        df = df.drop(df.columns[columns_to_drop], axis=1)
-
-        print("Columns dropped:")
-        print("Updated DataFrame:")
-        print(df.head(10))
-
-        # Prompt user if they want to drop more columns
-        drop_columns_option = input("Do you want to drop any other columns? (y/n): ")
-
-    display_dataframe_info(df)  # Display DataFrame info after column dropping
-
-    # Check for duplicate rows
-    duplicate_rows = df.duplicated()
-    if duplicate_rows.any():
-        print("Duplicate rows detected.")
-        handle_duplicates = input("How do you want to handle duplicate rows? (1. Remove, 2. Keep): ")
-        if handle_duplicates == "1":
-            print("Removing duplicate rows.")
-            df = df[~duplicate_rows]
-        else:
-            print("Keeping duplicate rows.")
-
-    display_dataframe_info(df)  # Display DataFrame info after handling duplicates
-
-    # Check for blank/null values
-    blank_values = df.isnull().sum()
-    if blank_values.any():
-        print("Blank/Null values detected.")
-        handle_blanks = input("How do you want to handle blank/null values? (1. Remove rows, 2. Fill with values): ")
-        if handle_blanks == "1":
-            print("Removing rows with blank/null values.")
-            df = df.dropna()
-        else:
-            print("Filling blank/null values with appropriate values.")
-            # Fill blank/null values using suitable techniques
-
-    display_dataframe_info(df)  # Display DataFrame info after handling blanks
-
-    return df
+    def handle_blank_values(self, remove_blanks):
+        data = self.data_table.items
+        if remove_blanks:
+            data = data.dropna()
+        self.show_data(data)
