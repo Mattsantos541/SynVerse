e20d3420ee35920f5f7e7a093c4d2534b5792920
Matt Santos <mattsantos541@gmail.com>
2023-12-18 09:44:49 -0600

    finish.py updated

--- my_app/finish.py
+++ my_app/finish.py
@@ -1,60 +1,28 @@
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.mime.base import MIMEBase
-from email import encoders
-import getpass
-import os
+import anvil.server
 
 
 class Finish:
-    def __init__(self, synthetic_dataset, scorecard_results):
+    def __init__(self, synthetic_dataset):
         self.synthetic_dataset = synthetic_dataset
-        self.scorecard_results = scorecard_results
 
     def generate_csv(self, filename):
-        self.synthetic_dataset.to_csv(filename, index=False)
-        print(f"Synthetic dataset saved as {filename}")
+        # Save the synthetic dataset as a CSV file in Anvil's built-in data storage
+        synthetic_data_table = anvil.server.tables.get_table('SyntheticData')  # Adjust table name
+        new_row = synthetic_data_table.add_row(SyntheticData=anvil.server.serialise(self.synthetic_dataset))
+        synthetic_data_table.update_data()
 
-    def send_email(self, sender_email, receiver_email, subject, body, attachment_path):
-        # Create a multipart message and set the email headers
-        message = MIMEMultipart()
-        message["From"] = sender_email
-        message["To"] = receiver_email
-        message["Subject"] = subject
+        # Return a download link for the user
+        download_link = anvil.server.call('generate_download_link', new_row['SyntheticData'], filename)
+        return download_link
 
-        # Add the body to the email
-        message.attach(MIMEText(body, "plain"))
 
-        # Open the attachment file and add it to the email
-        with open(attachment_path, "rb") as attachment:
-            part = MIMEBase("application", "octet-stream")
-            part.set_payload(attachment.read())
+# Anvil server function to generate a download link for the CSV file
+@anvil.server.callable
+def generate_download_link(synthetic_data, filename):
+    synthetic_data = anvil.server.deserialise(synthetic_data)
 
-        # Encode the attachment and add a header
-        encoders.encode_base64(part)
-        part.add_header(
-            "Content-Disposition",
-            f"attachment; filename= {os.path.basename(attachment_path)}",
-        )
+    # Save the DataFrame as a CSV file
+    synthetic_data.to_csv(filename, index=False)
 
-        # Add the attachment to the email message
-        message.attach(part)
-
-        # Convert the message to a string and send the email
-        text = message.as_string()
-
-        smtp_server = "smtp.gmail.com"
-        smtp_port = 587
-
-        # Prompt the user for their email password
-        password = getpass.getpass("Enter your email password: ")
-
-        try:
-            with smtplib.SMTP(smtp_server, smtp_port) as server:
-                server.starttls()
-                server.login(sender_email, password)
-                server.sendmail(sender_email, receiver_email, text)
-            print("Email sent successfully!")
-        except Exception as e:
-            print("An error occurred while sending the email:", str(e))
+    # Return the download link for the user
+    return anvil.BlobMedia("text/csv", open(filename, "rb").read(), name=filename)
